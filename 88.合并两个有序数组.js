d6d33ff96c4be5404a97eb6d53f6568b97bdee17
czy <757930657@qq.com>
2019-06-06 23:56:27 +0800

    88

--- 88.合并两个有序数组.js
+++ 88.合并两个有序数组.js
@@ -2,14 +2,21 @@
  * @Author: 陈智焰 
  * @Date: 2019-06-06 23:07:59 
  * @Last Modified by: 陈智焰
- * @Last Modified time: 2019-06-06 23:27:38
+ * @Last Modified time: 2019-06-06 23:55:39
+ */
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-    if (m == 0) {
-        nums1.concat(nums2);
-        nums1.shift();
+    if(m ==0){
+       for(let i = 0;i<n;i++){
+           nums1[i] = nums2[i];
+       }
     }
-
     for (let j = 0; j < n; j++)
         for (let i = m - 1; i >= 0; i--) {
             if (nums2[j] >= nums1[i]) {
@@ -20,13 +27,13 @@ var merge = function (nums1, m, nums2, n) {
                 nums1[i + 1] = nums2[j];
                 break;
             }
-            if (i == 0) {
+            if (i == 0){
                 m++;
-                for (let l = m - 1; l > i; l--) {
+                for (let l = m - 1; l > i ; l--) {
                     nums1[l] = nums1[l - 1];
                 }
-                nums1[i] = nums2[j];
-                break;
+                nums1[i ] = nums2[j];
+                break;  
             }
         }
 };
