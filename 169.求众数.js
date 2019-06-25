245411f8101a2da3b6c843a095e3e295b8228103
czy <757930657@qq.com>
2019-06-25 21:07:10 +0800

    163.求众数.js 229.求众数-ii.js

--- 169.求众数.js
+++ 169.求众数.js
@@ -1,4 +1,10 @@
 /*
+ * @Author: 陈智焰 
+ * @Date: 2019-06-25 20:51:35 
+ * @Last Modified by:   陈智焰 
+ * @Last Modified time: 2019-06-25 20:51:35 
+ */
+/*
  * @lc app=leetcode.cn id=169 lang=javascript
  *
  * [169] 求众数
@@ -7,14 +13,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-//map法
 var majorityElement2 = function (nums) {
     let m = new Map();
     let half_n = nums.length >> 1;
     for (let value of nums) {
         if (!m.has(value)) {
             m.set(value, 0);
-        } 
+        }
         let count = m.get(value);
         ++count;
         if (count > half_n)
@@ -22,20 +27,20 @@ var majorityElement2 = function (nums) {
         m.set(value, count);
     }
 };
-
 //摩尔投票法
 var majorityElement = function (nums) {
-    let count = 0,n =0;
-    for(let value of nums){
-        if (count==0){
-            n=value;
-            count++;
-        }else if(value==n){
-            count++;
-        } else if (value != n) {
-            count--;
+    let n = 0;
+    let count = 0;
+    let m ;
+    for(let v of nums){
+        if(0 == count){
+            m = v;
+            count ++;
+        }else if(v == m){
+            count ++;
+        }else{
+            count --;
         }
     }
-    return n;
+    return m;
 };
-console.log(majorityElement([3, 3, 4]))
---
+++ 229.求众数-ii.js
@@ -0,0 +1,88 @@
+/*
+ * @Author: 陈智焰 
+ * @Date: 2019-06-25 20:51:29 
+ * @Last Modified by: 陈智焰
+ * @Last Modified time: 2019-06-25 21:00:19
+ */
+/*
+ * @lc app=leetcode.cn id=229 lang=javascript
+ *
+ * [229] 求众数 II
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var majorityElement2 = function (nums) {
+    let result = []
+    let m = new Map();
+    let n_3 = Math.floor(nums.length / 3);
+    for (let value of nums) {
+        if (!m.has(value)) {
+            m.set(value, 0)
+        }
+        let count = m.get(value);
+        count++;
+        if (count > n_3) {
+            count = -Infinity;
+            result.push(value);
+        }
+        m.set(value, count);
+    }
+    return result;
+};
+
+//摩尔投票法
+var majorityElement = function (nums) {
+    let a,b;
+    let countA = 0, countB = 0;
+    for (let v of nums) {
+        if (countA > 0 && countB > 0) {
+            if (v != a && v != b) {
+                countA--;
+                countB--;
+            } else if (v == a) {
+                countA++;
+            } else {
+                countB++;
+            }
+
+        } else if (countA > 0) {
+            if (a == v) {
+                countA++;
+            } else {
+                b = v;
+                countB++;
+            }
+        } else if (countB > 0) {
+            if (b == v) {
+                countA++;
+            } else {
+                a = v;
+                countA++
+            }
+        } else {
+            a = v;
+            countA++;
+            b =NaN;
+            //当a,b,票数都为0时,
+            //下一个人会先顶掉a,
+            //如果下一个人是最后一个人,则b会被保留,例子[0,-1,2,-1]
+        }
+    }
+    let res = []
+    //题目没有保证数组有众数,所以要自行判断
+    countA = 0, countB = 0;
+    for (let v of nums) {
+        if (v == a)
+            countA++;
+        if (v == b)
+            countB++
+    }
+    if (countA > nums.length / 3)
+        res.push(a)
+    if (countB > nums.length / 3)
+        res.push(b)
+    return res;
+};
+console.log(majorityElement([0,-1,2,-1]))
