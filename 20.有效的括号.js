1a098e80a60855b7bbf53f1903c534c5049039fd
chenzhiyan <chenzhiyan@weile.com>
2019-07-29 18:06:45 +0800

    20.有效的括号.js

--- 20.有效的括号.js
+++ 20.有效的括号.js
@@ -12,17 +12,22 @@ var isValid = function (s) {
     let m = new Map([[")", "("], ["}", "{"], ["]", "["]]);
     for (let v of s) {
         if (m.has(v)) {
-            let r = res.pop();
-            console.log(r)
-            if (r != m.get(v))
+            if (res.pop() != m.get(v))
                 return false;
         } else {
             res.push(v);
         }
     }
-    if (res.length == 0)
-        return true;
-    else
-        return false;
+    return res.length == 0;
 };
 
+var isValid2 = function(s) {
+    var st = []
+    for(var l of s)
+        if ((i="({[]})".indexOf(l))>-1)
+            if (st[st.length-1]+i===5)
+                st.length--;
+            else
+                st.push(i);
+    return st.length===0
+};
